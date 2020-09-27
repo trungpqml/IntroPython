e610620dbced35ce210319617f4c1b5f34027ae0
trungpq <trungpq.ml@gmail.com>
2020-09-27 18:25:30 +0700

    First server, client

---
+++ Django/Week1/http/server.py
@@ -0,0 +1,37 @@
+from socket import *
+
+
+def createServer():
+    """
+    docstring
+    """
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    try:
+        server_socket.bind(('localhost', 9000))
+        server_socket.listen(5)
+        while (1):
+            (client_socket, address) = server_socket.accept()
+
+            rd = client_socket.recv(5000).decode()
+            pieces = rd.split('\n')
+            if len(pieces) > 0:
+                print(pieces[0])
+
+            data = "HTTP/1.1 200 OK\r\n"
+            data += "Content-Type: text/html; charset=utf-8\r\n"
+            data += "\r\n"
+            data += "<html><body>Hello World</body></html>\r\n\r\n"
+            client_socket.sendall(data.encode())
+            client_socket.shutdown(SHUT_WR)
+
+    except KeyboardInterrupt:
+        print('\nShutting down...\n')
+    except Exception as exc:
+        print('Error:\n')
+        print(exc)
+
+    server_socket.close()
+
+
+print('Access http://localhost:9000')
+createServer()
